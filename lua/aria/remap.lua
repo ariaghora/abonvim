daf0791d788a8a2633c26e37bf6ed4f9257f55d5
ariaghora <hello@ghora.net>
2023-01-17 22:05:38 +0900

    better toggling

--- lua/aria/remap.lua
+++ lua/aria/remap.lua
@@ -2,9 +2,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("t", "jj", "<c-\\><c-n>")
-vim.keymap.set("t", "<Esc>", "<c-\\><c-n>")
+vim.keymap.set("t", "<Esc>", vim.cmd.ToggleTerm)
 
 -- leader commands
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>tt", vim.cmd.ToggleTerm)
+vim.keymap.set("t", "<leader>tt", vim.cmd.ToggleTerm)
